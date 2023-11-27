3115abbe93fd5908b9ed23463ab18ad4cc45d654
Peter Romianowski <pero@cling.com>
2023-11-27 19:54:56 +0100

    [nvim] Adapt the setup of lspconfig to the latest docs

--- nvim/lua/pero/plugins/lsp/lspconfig.lua
+++ nvim/lua/pero/plugins/lsp/lspconfig.lua
@@ -24,35 +24,43 @@ vim.diagnostic.config({
 -- Populate the quickfix list with all LSP errors.
 vim.keymap.set("n", "<leader>ga", vim.diagnostic.setqflist)
 
-local on_attach = function(client, bufnr)
-	-- Mappings.
-	local opts = { silent = true, buffer = bufnr }
-	vim.keymap.set("n", "<leader>G", vim.lsp.buf.declaration, opts)
-	vim.keymap.set("n", "<leader>g", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "gr", function()
-		vim.lsp.buf.references({ includeDeclaration = false })
-	end, opts)
-	vim.keymap.set("n", "<leader>d", function()
-		vim.diagnostic.open_float({ source = true })
-	end, opts)
-	vim.keymap.set("n", "[d", function()
-		vim.diagnostic.goto_prev({
-			severity = { min = vim.diagnostic.severity.WARN },
-		})
-	end, opts)
-	vim.keymap.set("n", "]d", function()
-		vim.diagnostic.goto_next({
-			severity = { min = vim.diagnostic.severity.WARN },
-		})
-	end, opts)
-	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
-	vim.keymap.set("n", "<leader>pr", vim.lsp.buf.format, opts)
-end
+-- Diagnostic keymaps.
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.goto_prev({
+		severity = { min = vim.diagnostic.severity.WARN },
+	})
+end)
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.goto_next({
+		severity = { min = vim.diagnostic.severity.WARN },
+	})
+end)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+vim.keymap.set("n", "<leader>d", function()
+	vim.diagnostic.open_float({ source = true })
+end)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+		-- Buffer local mappings.
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "<leader>G", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "<leader>g", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", function()
+			vim.lsp.buf.references({ includeDeclaration = false })
+		end, opts)
+		vim.keymap.set("n", "<leader>pr", vim.lsp.buf.format, opts)
+	end,
+})
 
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
@@ -60,13 +68,11 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 for _, server in ipairs({ "html", "yamlls", "pyright" }) do
 	lspconfig[server].setup({
 		capabilities = capabilities,
-		on_attach = on_attach,
 	})
 end
 
 lspconfig.cssls.setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 	settings = {
 		css = {
 			-- We have to disable validation because `cssls` complains about
@@ -78,7 +84,6 @@ lspconfig.cssls.setup({
 
 lspconfig.tailwindcss.setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 	root_dir = lspconfig.util.root_pattern(
 		"tailwind.config.js",
 		"tailwind.config.cjs",
@@ -96,7 +101,6 @@ typescript.setup({
 	server = {
 		capabilities = capabilities,
 		on_attach = function(client, bufnr)
-			on_attach(client, bufnr)
 			local opts = { silent = true, buffer = bufnr }
 			vim.keymap.set("n", "<leader>g", vim.lsp.buf.implementation, bufopts)
 			vim.keymap.set("n", "<leader>G", ":TypescriptGoToSourceDefinition<CR>", opts)
@@ -135,7 +139,6 @@ rust_tools.setup({
 
 	server = {
 		on_attach = function(client, bufnr)
-			on_attach(client, bufnr)
 			local opts = { silent = true, buffer = bufnr }
 			vim.keymap.set("n", "<leader>e", rust_tools.expand_macro.expand_macro, bufopts)
 			vim.keymap.set("n", "<leader>tt", rust_tools.runnables.runnables, opts)
