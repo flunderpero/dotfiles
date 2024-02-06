19646b256624f85a741728ce156e9b63e286e280
Peter Romianowski <pero@cling.com>
2024-02-06 16:35:00 +0100

    [nvim] Use `C-n` to enter normal mode in terminals

--- nvim/lua/pero/core/terminal.lua
+++ nvim/lua/pero/core/terminal.lua
@@ -1,8 +1,6 @@
 -- Automatically start terminals in insert mode.
 vim.api.nvim_create_autocmd("TermOpen", { pattern = "*", command = [[startinsert]] })
 
--- Switch to normal mode. We cannot use <C-\><C-n> because
--- we have mapped <C-n> in kitty.conf.
-vim.keymap.set("t", "<C-]>", function()
+vim.keymap.set("t", "<C-n>", function()
 	vim.cmd("stopinsert")
 end, { silent = true })
