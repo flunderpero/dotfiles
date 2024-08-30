ed07d945e8a58a0645a93b6f743dc281a1ee6467
Peter Romianowski <pero@cling.com>
2024-08-30 23:24:40 +0200

    [nvim] Don't enter insert mode on new terminal windows

--- nvim/lua/pero/core/terminal.lua
+++ nvim/lua/pero/core/terminal.lua
@@ -1,6 +1,3 @@
--- Automatically start terminals in insert mode.
-vim.api.nvim_create_autocmd("TermOpen", { pattern = "*", command = [[startinsert]] })
-
 vim.keymap.set("t", "<C-n>", function()
 	vim.cmd("stopinsert")
 end, { silent = true })
