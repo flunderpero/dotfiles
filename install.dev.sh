6b9bbfd3bee2288ef3b43ff898f7658832a60858
Peter Romianowski <pero@cling.com>
2025-03-19 11:13:58 +0100

    [install] Enable Git commit signing

--- install.dev.sh
+++ install.dev.sh
@@ -3,3 +3,8 @@
 brew install nvm vips imagemagick graphicsmagick stern protobuf protobuf@3
 
 brew tap homebrew/cask-fonts
+
+echo "Enabling git commit signing..."
+git config --global gpg.format ssh
+git config --global user.signingkey ~/.ssh/id_rsa_pero_flunder_io_github.pub
+git config --global commit.gpgsign true
